ee0f45c09200e3e785e042c3870a71c55bfc6ca2
lucas augusto <luaugustomonteiror@gmail.com>
2026-09-11 20:00:33 -0300

    ImitandoDory

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("--Tradutor de Baleias--\n");
+Console.Write("Em Português: ");
+
+string texto = Console.ReadLine()!;
+Console.WriteLine("\nEm Baleês:\n");
+
+
+
+Console.WriteLine(texto
+.Replace("a", "aaa")
+.Replace("e", "eee")
+.Replace("i", "iii")
+.Replace("o", "ooo")
+.Replace("u", "uuu")
+.Replace("A", "AAA")
+.Replace("E", "EEE")
+.Replace("I", "III")
+.Replace("O", "OOO")
+.Replace("U", "UUU")
+);
+Console.WriteLine();
